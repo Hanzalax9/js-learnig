96ac9326796ae7c89413a84db3f5b796c3bfec78
Hanzalax9 <hanzala.work@gmail.com>
2024-12-19 13:59:21 +0500

    if else

--- basic2.js
+++ basic2.js
@@ -1,5 +1,13 @@
-const firstName = "Hanzala"
-const second = "Sarwar"
-
-console.log("Your first name is", firstName, "and second name is",second)
-console.log(`Your first name is ${firstName} and second name is ${second}`)
+const firstName = "Hanzala";
+const second = "Sarwar";
+const subjectMarks = [50, 30, 60, 70];
+const age = 18;
+console.log("Your first name is", firstName, "and second name is", second);
+console.log(`Your first name is ${firstName} and second name is ${second}`);
+if (age > 18) {
+  console.log("You're adult");
+} else if (age === 18) {
+  console.log("you are 18");
+} else {
+  console.log("You are kid");
+}
