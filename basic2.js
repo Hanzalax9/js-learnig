6dc98aed88e6e6733f401f58bb07c56ef5d49fca
Hanzalax9 <hanzala.work@gmail.com>
2024-12-19 13:51:02 +0500

    Qoutation

---
+++ basic2.js
@@ -0,0 +1,5 @@
+const firstName = "Hanzala"
+const second = "Sarwar"
+
+console.log("Your first name is", firstName, "and second name is",second)
+console.log(`Your first name is ${firstName} and second name is ${second}`)
--- basics.js
+++ basics.js
@@ -1,6 +1,5 @@
 const accountId = 12312;
 let accountEmail = "Hanzala@gmail.com";
 accountEmail= "Hanzala"
-
-
 console.log(accountEmail);
+console.table([accountEmail, accountId])
